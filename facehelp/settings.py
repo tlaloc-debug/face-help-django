f15a19bde210120dfbda8744fa84b77d023f32d5
tlaloc_debug <pelondinho@hotmail.com>
2021-09-11 14:48:56 -0400

    new commit

--- facehelp/settings.py
+++ facehelp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jc47^=!x7b&(g2u#v(pr0&zou8n_j$(^d!@6@cqulq*1oq*9-g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['face-help-django.herokuapp.com']
+ALLOWED_HOSTS = ['face-help-django.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
